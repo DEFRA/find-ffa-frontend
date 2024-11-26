23959e2cc63c5d037b613fcaf42ca63ae07fd05d
Ben Wilkes <ben@eclipseitconsulting.co.uk>
2024-11-26 09:59:21 +0000

    attempting to get it working in CDP #4

--- app/index.js
+++ app/index.js
@@ -1,18 +1,28 @@
+const process = require('node:process')
 const createServer = require('./server')
 const { getLogger } = require('./lib/logger')
 
 const init = async () => {
-  const server = await createServer()
-  await server.start()
-  server.logger.info('Server started successfully')
-  server.logger.info(`Server running on ${server.info.uri}`)
+  try {
+    const server = await createServer()
+    await server.start()
+
+    server.logger.info('Server started successfully')
+    server.logger.info(`Server running on ${server.info.uri}`)
+
+    return server
+  } catch (error) {
+    const logger = getLogger()
+    logger.info('Server failed to start :(')
+    logger.error(error)
+  }
 }
 
 init().catch((err) => {
   const logger = getLogger()
   logger.info('Server failed to start :(')
   logger.error(err)
-  process.exit(1)
+  process.exitCode = 1
 })
 
 process.on('unhandledRejection', (error) => {
