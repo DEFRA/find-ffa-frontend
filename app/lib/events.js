2617913791d37654daef103a66f48a78b7c47454
Ben Wilkes <ben@eclipseitconsulting.co.uk>
2024-11-28 12:11:09 +0000

    cleaning up logging #1

--- app/lib/events.js
+++ app/lib/events.js
@@ -17,7 +17,7 @@ const Event = {
  * @param {{message: string, conversationId: string}} eventProps
  */
 const trackMessage = (eventProps) => {
-  logger.debug(Event.USER_MESSAGE, eventProps)
+  logger.debug(`${Event.USER_MESSAGE} - ${JSON.stringify(eventProps)}`)
 }
 
 /**
@@ -25,27 +25,27 @@ const trackMessage = (eventProps) => {
  * @param {{message: string, conversationId: string}} eventProps
  */
 const trackSystemMessage = (eventProps) => {
-  logger.debug(Event.SYSTEM_MESSAGE, eventProps)
+  logger.debug(`${Event.SYSTEM_MESSAGE} - ${JSON.stringify(eventProps)}`)
 }
 
 const trackLandingPageView = (conversationId) => {
-  logger.debug(Event.LANDING_PAGE_VIEW, { time: new Date(), conversationId })
+  logger.debug(`${Event.LANDING_PAGE_VIEW} - ${conversationId}`)
 }
 
 const trackConversationPageView = (conversationId) => {
-  logger.debug(Event.CONVERSATION_PAGE_VIEW, { time: new Date(), conversationId })
+  logger.debug(`${Event.CONVERSATION_PAGE_VIEW} - ${conversationId}`)
 }
 
 const trackHallucinatedLinkInResponse = ({ requestQuery, errorMessage, failedObject }) => {
-  logger.debug(Event.HALLUCINATED_LINK, { time: new Date(), requestQuery, errorMessage, failedObject })
+  logger.debug(`${Event.HALLUCINATED_LINK} - ${JSON.stringify(requestQuery)} - ${errorMessage} - ${JSON.stringify(failedObject)}`)
 }
 
 const trackFetchResponseFailed = ({ requestQuery, errorMessage, retryCount }) => {
-  logger.debug(Event.FETCH_RESPONSE_FAILED, { time: new Date(), requestQuery, errorMessage, retryCount })
+  logger.debug(`${Event.FETCH_RESPONSE_FAILED} - ${JSON.stringify(requestQuery)} - ${errorMessage} - ${retryCount}`)
 }
 
 const trackCacheUpload = ({ requestQuery }) => {
-  logger.debug(Event.CACHE_UPLOAD, { time: new Date(), requestQuery })
+  logger.debug(`${Event.CACHE_UPLOAD} - ${requestQuery}`)
 }
 
 module.exports = {
